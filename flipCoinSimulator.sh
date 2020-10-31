76182ebca5c3a323e5bad9bfe5ffe3cb0cd916b0
Yash <yashn49@gmail.com>
2020-10-31 06:35:43 +0530

    headTail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo "welcome o flip coin simulator program"
+isHeads=0;
+isTails=1;
+coinFlip=$((RANDOM%2))
+if [ $isHeads -eq $coinFlip ]
+ then
+  echo "heads is winner"
+ else
+  echo "tails is winner"
+fi
